c1f5a34449b6640e52151703b7797e5defa9c9cd
Nuth68 <pravevinuth888@gmail.com>
2025-11-17 13:24:22 +0700

    Update Code

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,65 @@
 <template>
-  <div class="container">
-    <CategoryList />
-    <PromoCards />
+  <div class="home">
+
+    <CategoryListLocal :categories="categories" />
+    <PromotionListLocal :promotions="promotions" />
   </div>
 </template>
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import CategoryList from './components/CategoryList.vue'
-import PromoCards from './components/PromoCards.vue'
 
+<script>
+import axios from "axios";
+import CategoryList from "./components/CategoryCompoenet.vue";
+import PromotionList from "./components/PromoCard.vue";
+
+export default {
+  name: "Home",
+
+  components: {
+    CategoryListLocal: CategoryList,
+    PromotionListLocal: PromotionList,
+  },
+
+  data() {
+    return {
+      categories: [], 
+      promotions: []  
+    };
+  },
+
+  methods: {
+    async fetchCategories() {
+      try {
+        const res = await axios.get("http://localhost:3000/api/categories");
+        this.categories = res.data;   
+        console.log("Loaded categories:", this.categories);
+      } catch (err) {
+        console.error("Error loading categories:", err);
+      }
+    },
+
+    async fetchPromotions() {
+      try {
+        const res = await axios.get("http://localhost:3000/api/promotions");
+        this.promotions = res.data;
+        console.log("Loaded promotions:", this.promotions);
+      } catch (err) {
+        console.error("Error loading promotions:", err);
+      }
+    },
+  },
+
+  mounted() {
+   
+    this.fetchCategories();
+    this.fetchPromotions();
+  }
+};
 </script>
+
+<style scoped>
+.home {
+  max-width: 1200px;
+  margin: auto;
+  padding: 40px 20px;
+}
+</style>
